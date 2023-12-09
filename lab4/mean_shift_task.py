d95eecda456cba5dda0020ffc861d89162f0792c
Никита Носов <darkxaril@yandex.ru>
2023-12-09 12:45:14 +0400

    fast

--- lab4/mean_shift_task.py
+++ lab4/mean_shift_task.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 
 STOP_THRESHOLD = 1e-4
 CLUSTER_THRESHOLD = 1e-1
+
 
 def gaussian_cluster(cx: float = 0.0, cy: float = 0.0, sigma_x: float = 0.1, sigma_y: float = 0.1, n_points: int = 1024):
     """
@@ -12,11 +14,14 @@ def gaussian_cluster(cx: float = 0.0, cy: float = 0.0, sigma_x: float = 0.1, sig
     return np.hstack((np.random.normal(cx, sigma_x, n_points).reshape((n_points, 1)),
                       np.random.normal(cy, sigma_y, n_points).reshape((n_points, 1))))
 
+
 def distance(a, b):
     return np.linalg.norm(np.array(a) - np.array(b))
 
+
 def gaussian_kernel(distance, bandwidth):
     return (1 / (bandwidth * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((distance / bandwidth)) ** 2)
+
 
 class MeanShift(object):
     def __init__(self, kernel=gaussian_kernel):
@@ -33,29 +38,24 @@ class MeanShift(object):
                 if not shifting[i]:
                     continue
                 p_shift_init = shift_points[i].copy()
-                shift_points[i] = self._shift_point(shift_points[i], points, kernel_bandwidth)
+                shift_points[i] = self._shift_point(
+                    shift_points[i], points, kernel_bandwidth)
                 dist = distance(shift_points[i], p_shift_init)
                 max_dist = max(max_dist, dist)
                 shifting[i] = dist > STOP_THRESHOLD
 
-            if(max_dist < STOP_THRESHOLD):
+            if (max_dist < STOP_THRESHOLD):
                 break
         cluster_ids = self._cluster_points(shift_points.tolist())
         return shift_points, cluster_ids
 
     def _shift_point(self, point, points, kernel_bandwidth):
-        shift_x = 0.0
-        shift_y = 0.0
-        scale = 0.0
-        for p in points:
-            dist = distance(point, p)
-            weight = self.kernel(dist, kernel_bandwidth)
-            shift_x += p[0] * weight
-            shift_y += p[1] * weight
-            scale += weight
-        shift_x = shift_x / scale
-        shift_y = shift_y / scale
-        return [shift_x, shift_y]
+        points = np.array(points)
+        dist = np.linalg.norm(points - point, axis=1)
+        weight = self.kernel(dist, kernel_bandwidth)
+        scale = np.sum(weight)
+        shift = (weight @ points) / scale
+        return shift
 
     def _cluster_points(self, points):
         cluster_ids = []
@@ -63,33 +63,34 @@ class MeanShift(object):
         cluster_centers = []
 
         for i, point in enumerate(points):
-            if(len(cluster_ids) == 0):
+            if (len(cluster_ids) == 0):
                 cluster_ids.append(cluster_idx)
                 cluster_centers.append(point)
                 cluster_idx += 1
             else:
                 for center in cluster_centers:
                     dist = distance(point, center)
-                    if(dist < CLUSTER_THRESHOLD):
+                    if (dist < CLUSTER_THRESHOLD):
                         cluster_ids.append(cluster_centers.index(center))
-                if(len(cluster_ids) < i + 1):
+                if (len(cluster_ids) < i + 1):
                     cluster_ids.append(cluster_idx)
                     cluster_centers.append(point)
                     cluster_idx += 1
         return cluster_ids
 
 
-import matplotlib.pyplot as plt 
-
 def colors(n):
-  ret = []
-  for i in range(n):
-    ret.append((random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)))
-  return ret
+    ret = []
+    for i in range(n):
+        ret.append((random.uniform(0, 1), random.uniform(
+            0, 1), random.uniform(0, 1)))
+    return ret
+
 
 def debuf_color(color):
-    debuf_color = tuple(c * 0.8 for c in color)
+    debuf_color = tuple(c * 0.7 for c in color)
     return debuf_color
+
 
 def separated_clusters():
     """
@@ -103,14 +104,16 @@ def separated_clusters():
                                gaussian_cluster(cx=2.5, n_points=n)))
     mean_shifter = MeanShift()
 
-    shift_points, mean_shift_result = mean_shifter.fit(clusters_data, kernel_bandwidth=0.15)
-
+    shift_points, mean_shift_result = mean_shifter.fit(
+        clusters_data, kernel_bandwidth=0.15)
 
     np.set_printoptions(precision=3)
     color = colors(np.unique(mean_shift_result).size)
     for i in range(len(mean_shift_result)):
-        plt.scatter(clusters_data[i, 0], clusters_data[i, 1], color = color[mean_shift_result[i]])
-        plt.scatter(shift_points[i][0], shift_points[i][1], color = debuf_color(color[mean_shift_result[i]]), marker='x')
+        plt.scatter(clusters_data[i, 0], clusters_data[i,
+                    1], color=color[mean_shift_result[i]])
+        plt.scatter(shift_points[i][0], shift_points[i][1], color=debuf_color(
+            color[mean_shift_result[i]]), marker='x')
     plt.show()
 
 
@@ -120,13 +123,16 @@ def merged_clusters():
     """
     clusters_data = gaussian_cluster(n_points=100)
     mean_shifter = MeanShift()
-    shift_points, mean_shift_result = mean_shifter.fit(clusters_data, kernel_bandwidth=0.15)
+    shift_points, mean_shift_result = mean_shifter.fit(
+        clusters_data, kernel_bandwidth=0.15)
 
     np.set_printoptions(precision=3)
     color = colors(np.unique(mean_shift_result).size)
     for i in range(len(mean_shift_result)):
-        plt.scatter(clusters_data[i, 0], clusters_data[i, 1], color = color[mean_shift_result[i]])
-        plt.scatter(shift_points[i][0], shift_points[i][1], color = debuf_color(color[mean_shift_result[i]]), marker='x')
+        plt.scatter(clusters_data[i, 0], clusters_data[i,
+                    1], color=color[mean_shift_result[i]])
+        plt.scatter(shift_points[i][0], shift_points[i][1], color=debuf_color(
+            color[mean_shift_result[i]]), marker='x')
     plt.show()
 
 
